71a37765646d84c0ea32fe8179607c318b30194b
Valeria Galaviz Rodriguez <valeriagalavizrodriguez@gmail.com>
2025-03-01 13:32:23 -0600

    Práctica tipos de datos

---
+++ 1-Datos-js/tipos-de-dato.js
@@ -0,0 +1,27 @@
+//Tipos de datos en JavaScript
+
+console.log("10 es un tipo de dato:", typeof 10); //number
+
+console.log("Veinticinco es un tipo de dato:", typeof 'Veinticinco'); //string
+
+console.log("-666 es un tipo de dato:", typeof -666); //number
+
+console.log("true es un tipo de dato:", typeof tru); //boolean
+
+console.log("0 es un tipo de dato:", typeof 0); //number
+
+console.log("'' es un tipo de dato:", typeof ''); //string
+
+console.log("null es un tipo de dato:", typeof null); //object
+
+console.log("undefined es un tipo de dato:", typeof undefined); //undefined
+
+console.log("FALSE es un tipo de dato:", typeof FALSE); //undefined
+
+console.log("false es un tipo de dato:", typeof false); //boolean
+
+console.log("NaN es un tipo de dato:", typeof NaN); //number
+
+console.log("56.47 es un tipo de dato:", typeof 56.47); //number
+
+
